39e7cfafe554051aa465599d7c6f4820211305bf
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-21 11:54:44 +0100

    Add button one

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
-document.querySelector("#btn").addEventListener('click', function() {
-    const burst = new mojs.Burst({
-        radius: { 0 : 360},
-    }).play();
+const btn1 = document.querySelector(".btn1");
+
+const burst1 = new mojs.Burst({
+
+    radius:   { 0: 100 },
+    count:    5,
+    children: {
+      shape:        'circle',
+      radius:       20,
+      fill:         [ 'deeppink', 'cyan', 'yellow' ],
+      strokeWidth:  5,
+      duration:     2000
+    }
+  });
+
+btn1.addEventListener('click', function (e) {
+    burst1
+    .tune({ x: e.pageX, y: e.pageY  })
+    .play();
 });
+
